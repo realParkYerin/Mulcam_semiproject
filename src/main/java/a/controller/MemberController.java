47a526df717b47d97c11046b510543854977a406
Dev_Dayun <devdayun11@gmail.com>
2023-03-16 15:31:38 +0900

    fix : 회원가입 중 프로필 사진 저장 위치 로컬로 변경

--- src/main/java/a/controller/MemberController.java
+++ src/main/java/a/controller/MemberController.java
@@ -74,37 +74,50 @@ public class MemberController {
 							Model model) {
 		System.out.println("MemberController registerAf " + new Date());
 		
-		// filename 취득 : 원본 파일명
-		String filename = memberImg.getOriginalFilename();
-		
-		// upload의 경로 설정(2가지 : server / folder)
-		// server
-		String filepath = req.getServletContext().getRealPath("/memberImgs");
-		
-		// 파일명을 충돌되지 않는 명칭(Date)으로 변경
-		String newfilename = PdsUtil.getNewFileName(filename);
-		
-		// 변경된 파일명
-		memDto.setImg_path(newfilename);
-		
-		File file = new File(filepath + "/" + newfilename);
-		
-		try {
-			// 실제로 파일 생성 + 기입 = 업로드
-			FileUtils.writeByteArrayToFile(file, memberImg.getBytes());
+		// 1. 사용자가 프로필 사진을 업로드 했는지 확인
+		if(!memberImg.isEmpty()) { // 업로드한 파일이 존재
 			
-			boolean isS = memService.addMember(memDto);
-			String msg = "";
-			if(isS == true) {
-				msg = "MEMBER_ADD_YES";
-			}else {
-				msg = "MEMBER_ADD_NO";
+			// filename 취득 : 원본 파일명
+			String filename = memberImg.getOriginalFilename();
+			System.out.println("original: " + filename);
+			
+			// upload의 경로 설정(2가지 : server / folder)
+			// folder
+			String filepath = "c:\\memberImgs";
+			System.out.println("fupload: " + filepath);
+			
+			// 파일명을 충돌되지 않는 명칭(Date)으로 변경
+			String newfilename = PdsUtil.getNewFileName(filename);
+			
+			// 변경된 파일명
+			memDto.setImg_path(newfilename);
+			
+			File file = new File(filepath + "/" + newfilename);
+			try {
+				// 실제로 파일 생성 + 기입 = 업로드
+				FileUtils.writeByteArrayToFile(file, memberImg.getBytes());			
+				
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			model.addAttribute("register", msg);	
 			
-		} catch (IOException e) {
-			e.printStackTrace();
+		} else { // 업로드한 파일 미존재시 기본 사진 정보를 DB에 저장
+			
+			 // 기본 프로필이 있는 경로를 DB에 저장
+			String filepath = "C:" + File.separator + "memberImgs" + File.separator +"default_profile.png";
+			memDto.setImg_path(filepath);
+			 
 		}
+		
+		boolean isS = memService.addMember(memDto);
+		String msg = "";
+		if(isS == true) {
+			msg = "MEMBER_ADD_YES";
+		}else {
+			msg = "MEMBER_ADD_NO";
+		}
+		model.addAttribute("register", msg);	
+		
 		
 		return "message";
 					
